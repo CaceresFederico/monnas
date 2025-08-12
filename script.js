805b23054e8e4c8152f612def6f7be919f55bb53
Federico <caceresfederico@hotmail.com>
2025-08-12 01:49:18 -0300

    Nav items now auto adjust y position, nav-links new style display mobile, ionicicons librarie removed + performance, some css clases improvement.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const menuIcon = document.querySelector('.menu-icon');
-const navLinks = document.querySelector('.nav-links');
-menuIcon.addEventListener('click', () => {
-    console.log(menuIcon);
-    console.log(navLinks);
-    navLinks.classList.toggle('active');
-})
+const menuToggle = document.querySelector(".menu-icon");
+const navLinks = document.querySelector(".nav-links");
+const header = document.querySelector(".navbar");
+//   CURRENT VIEWPORT SIZE
+const currentViewportSize = window.innerWidth;
+console.log(currentViewportSize)
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  //   NAV HEIGHT
+  const navHeight = header.offsetHeight;
+  //   AUTO ADJUST NAV LINKS MARGIN TOP
+  navLinks.style.marginTop = `${navHeight}px`;
+});
+

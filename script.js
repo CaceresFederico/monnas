4fa29695d19ec29dfedff42885330c05c2f337f0
Federico <caceresfederico@hotmail.com>
2025-08-12 18:51:44 -0300

    Menu icon removed navlinks now are visible on sm resolutions, Bootstrap removed + performance, Shop icon enhanced style

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const menuToggle = document.querySelector(".menu-icon");
-const navLinks = document.querySelector(".nav-links");
-const header = document.querySelector(".navbar");
-//   CURRENT VIEWPORT SIZE
-const currentViewportSize = window.innerWidth;
-console.log(currentViewportSize)
 
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-  //   NAV HEIGHT
-  const navHeight = header.offsetHeight;
-  //   AUTO ADJUST NAV LINKS MARGIN TOP
-  navLinks.style.marginTop = `${navHeight}px`;
-});
+
 
